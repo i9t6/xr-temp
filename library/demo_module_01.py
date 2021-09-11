2844094fb1658eb61927f968f93c2195a6f62f54
i9t6 <frquiroz@hotmail.com>
2021-09-10 19:34:14 -0500

    Update demo_module_01.py

--- library/demo_module_01.py
+++ library/demo_module_01.py
@@ -43,60 +43,52 @@ def main():
         argument_spec=dict(
             var1=dict(type='list', required=True),
             var2=dict(type='list', required=True),
-            var3=dict(type='list', required=True),
-            var4=dict(type='dict', required=True)
+            var3=dict(type='dict', required=True)
         ),
         supports_check_mode=True
     )
 
     dic_temp = {}
     for i in module.params['var1']:
-        try:
-            int_var=re.match(r'Bundle-Ether[0-9]+\.[0-9]+',i).group(0) 
-        except:
-            continue
-        else:
-            dic_temp[int_var]={}
-
-    for i in module.params['var2']:
+        flag = 0
+        dic_temp[i['item']]={}
         for l in i["stdout_lines"][0]:
-            if "Mbps-IN" in l:
-                dic_temp[i['item']]['in_old']=re.findall(r"[0-9]+Mbps-IN",l)[0]
-            elif "Mbps-OUT" in l:
-                dic_temp[i['item']]['out_old']=re.findall(r"[0-9]+Mbps-OUT",l)[0]
-            elif "Gbps-IN" in l:
-                dic_temp[i['item']]['in_old']=re.findall(r"[0-9]+Gbps-IN",l)[0]
-            elif "Gbps-OUT" in l:
-                dic_temp[i['item']]['out_old']=re.findall(r"[0-9]+Gbps-OUT",l)[0]
-            elif "No policy-map" in l:
-                if 'input' in l:
-                    dic_temp[i['item']]['in_old']='tbd'
-                elif 'output' in l:
-                    dic_temp[i['item']]['out_old']='tbd'
+            if not "No policy-map" in l:
+                for n in ["Mbps-IN","Mbps-OUT","Gbps-IN","Gbps-OUT"]:
+                    if n in l:
+                        dic_temp[i['item']]['in_old']=re.findall(rf"[0-9]+{n}",l)[0]
+            elif 'input' in l:
+                dic_temp[i['item']]['in_old']='tbd'
+                flag += 1
+            elif 'output' in l:
+                dic_temp[i['item']]['out_old']='tbd'
+                flag += 1
+        #dic_temp[i['item']]['flag'] = flag
+        if flag == 2:
+            dic_temp[i['item']] = 'NA'
     
-    for i in module.params['var3']:
+    for i in module.params['var2']:
         try:
             int=re.findall(r'BE([0-9]+\.[0-9]+)',i)[0]
             #int = re.sub("\s+", ",", i).split(',')[0]
         except:
             continue
-        try:
-            dic_temp['Bundle-Ether'+int]['description'] = re.findall(r"\*\*\*\*\* ([\w_\.\s]+)",i)[0]
-        except:
-            dic_temp['Bundle-Ether'+int]['description'] = "Sin descripcion"
+        if not dic_temp['Bundle-Ether'+int] == 'NA':
+            try:
+                dic_temp['Bundle-Ether'+int]['description'] = re.findall(r"\*\*\*\*\* ([\w_\.\s]+)",i)[0]
+            except:
+                dic_temp['Bundle-Ether'+int]['description'] = "Sin descripcion"
+        else:
+            dic_temp.pop('Bundle-Ether'+int)
 
-    for i in dic_temp.keys():
-        dic_temp[i]['in_new']=module.params['var4'][dic_temp[i]['in_old']]
-        dic_temp[i]['out_new']=module.params['var4'][dic_temp[i]['out_old']]
-
-
-
+    for key,value in dic_temp.items():
+        value['in_new']=module.params['var3'][value['in_old']]
+        value['out_new']=module.params['var3'][value['out_old']]
 
     if module.check_mode:
         module.exit_json(changed=False)
 
     module.exit_json(meta=dic_temp)
-
 
 if __name__ == '__main__':
     """Execute main program."""
